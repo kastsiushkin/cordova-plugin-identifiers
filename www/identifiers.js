b976ea05f8855a03557fb66dd2fe9fe3e3180a01
kastsiushkin <alexkastsiushkin@gmail.com>
2015-12-16 15:17:19 -0500

    Create an Object

--- www/identifiers.js
+++ www/identifiers.js
@@ -1,10 +1,3 @@
-/**
-* Created with JetBrains WebStorm.
-* User: user
-* Date: 1/17/14
-* Time: 3:05 PM
-* To change this template use File | Settings | File Templates.
-*/
 var exec = require('cordova/exec');
 
 var IdentifiersPlugin = function() {};
@@ -15,6 +8,6 @@ IdentifiersPlugin.prototype.getIDFA = function (success, failure) {
   exec(success, failure, "IdentifiersPlugin", "advertisingIdentifier", []);
 };
 
-var identifiers = IdentifiersPlugin();
+var identifiers = new IdentifiersPlugin();
 
 module.exports = identifiers;
