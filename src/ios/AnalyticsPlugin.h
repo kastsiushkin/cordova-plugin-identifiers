22afb47483c2cceba6257739875ef51f2ef8402a
kastsiushkin <alexkastsiushkin@gmail.com>
2015-12-16 10:10:40 -0500

    Remove

--- src/ios/AnalyticsPlugin.h
+++ src/ios/AnalyticsPlugin.h
@@ -2,7 +2,6 @@
 Alvaro Rojas
  */
 
-#import <Foundation/Foundation.h>
 #import <Cordova/CDVPlugin.h>
 
 @interface AnalyticsPlugin : CDVPlugin {
@@ -10,21 +9,4 @@ Alvaro Rojas
 }
 
 - (void)getDeviceValues:(CDVInvokedUrlCommand*)command;
-
-
-
-//Get values from mobile devices
-- (NSString*)iOSAppRelease;
-- (NSString*)iOSDeviceUDID;
-- (NSString*)iOSDeviceName;
-- (NSString*)iOSDeviceSystemName;
-- (NSString*)iOSDeviceOSVersion;
-- (NSString*)iOSDeviceModel;
-- (NSString*)iOSCarrier;
-- (NSString*)iOSIfa;
-- (NSString*)iOSIfv;
-- (NSString*)iOSScreenWidth;
-- (NSString*)iOSScreenHeight;
-- (NSString*)iOSWiFiConnection;
-
 @end
