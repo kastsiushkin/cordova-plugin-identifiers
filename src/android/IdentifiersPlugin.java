f280b821e2d4948f33d41ca8c4bfc6908e95bc79
kastsiushkin <alexkastsiushkin@gmail.com>
2015-12-17 13:19:47 -0500

    Rename file

--- src/android/IdentifiersPlugin.java
+++ src/android/IdentifiersPlugin.java
@@ -1,12 +1,22 @@
 package com.segment.cordova;
 
 import android.util.Log;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.nfc.NfcAdapter;
+import android.os.Build;
+import android.telephony.TelephonyManager;
+import android.util.DisplayMetrics;
 import android.provider.Settings.Secure;
 
-import org.apache.cordova.BuildConfig;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class IdentifiersPlugin extends CordovaPlugin {
   public String advertisingIdentifier()
