205b0dfd4171b98356a2aebab393f9a80282b6b0
kastsiushkin <alexkastsiushkin@gmail.com>
2015-12-17 16:26:37 -0500

    Remove colon

--- src/ios/IdentifiersPlugin.m
+++ src/ios/IdentifiersPlugin.m
@@ -13,16 +13,16 @@
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
-- (NSString*)getIdfa:(CDVInvokedUrlCommand*)command {
+- (NSString*)getIdfa(CDVInvokedUrlCommand*)command {
   NSString *uniqueString = [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
   return [self validateString:uniqueString];
 }
 
-- (NSString*)getIdfv:(CDVInvokedUrlCommand*)command {
+- (NSString*)getIdfv(CDVInvokedUrlCommand*)command {
   return [self validateString:[[[UIDevice currentDevice] identifierForVendor] UUIDString]];
 }
 
-- (NSString*)validateString:(NSString*)str {
+- (NSString*)validateString(NSString*)str {
     return str == nil? @"":str;
 }
 
