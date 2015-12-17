91e9b3ca4e22ce7479e7356bce17b52ff5357092
kastsiushkin <alexkastsiushkin@gmail.com>
2015-12-17 14:44:49 -0500

    Fix typo

--- src/android/IdentifiersPlugin.java
+++ src/android/IdentifiersPlugin.java
@@ -43,7 +43,7 @@ public class IdentifiersPlugin extends CordovaPlugin {
     public String getAdid() {
         AdvertisingIdClient.Info advId;
         advId = AdvertisingIdClient.getAdvertisingIdInfo(cordova.getActivity());
-        return uuid;
+        return advId;
     }
 
 }
