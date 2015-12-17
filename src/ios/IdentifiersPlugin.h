c2b4b04dba81f864836982c5a9cbcaf0832f8181
kastsiushkin <alexkastsiushkin@gmail.com>
2015-12-17 15:55:11 -0500

    tryoing

--- src/ios/IdentifiersPlugin.h
+++ src/ios/IdentifiersPlugin.h
@@ -1,3 +1,4 @@
+#import <Foundation/Foundation.h>
 #import <AdSupport/ASIdentifierManager.h>
 #import <Cordova/CDVPlugin.h>
 
--- src/ios/IdentifiersPlugin.m
+++ src/ios/IdentifiersPlugin.m
@@ -1,4 +1,10 @@
 #import "IdentifiersPlugin.h"
+#import <CoreTelephony/CTTelephonyNetworkInfo.h>
+#import <CoreTelephony/CTCarrier.h>
+#import <AdSupport/ASIdentifierManager.h>
+#import "CDVReachability.h"
+#include <sys/types.h>
+#include <sys/sysctl.h>
 
 @implementation IdentifiersPlugin
 
