f8e67c6ac22fa67569713b0105f460393c4a565f
kastsiushkin <alexkastsiushkin@gmail.com>
2016-01-05 11:26:15 -0800

    Move initialization inside the plugin

--- www/identifiers.js
+++ www/identifiers.js
@@ -1,13 +1,26 @@
-var exec = require('cordova/exec');
+var exec = require('cordova/exec'),
+	channel = require('cordova/channel'),
+	utils = require('cordova/utils');
 
 var IdentifiersPlugin = function() {
   this.serviceName = 'IdentifiersPlugin';
+  this.values = null;
+
+  var me = this;
+
+  channel.onCordovaReady.subscribe(function() {
+        me.getDeviceValues(function(values) {
+        	me.values = values;
+            channel.onCordovaInfoReady.fire();
+        },function(e) {
+            me.available = false;
+            utils.alert("[ERROR] Error initializing Cordova: " + e);
+        });
+    });
 };
 
 IdentifiersPlugin.prototype.getDeviceValues = function (success, failure) {
   exec(success, failure, this.serviceName, "getDeviceValues", []);
 };
 
-var identifiers = new IdentifiersPlugin();
-
-module.exports = identifiers;
+module.exports = new IdentifiersPlugin();
